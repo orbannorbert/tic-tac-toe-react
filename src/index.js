14957279be8613bdf7900e41438a1874ed920f9e
orbannorbert <o.norbert.dev@icloud.com>
2020-02-17 15:09:52 +0100

    add style to html,body

--- src/index.js
+++ src/index.js
@@ -1,8 +1,24 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { createGlobalStyle } from 'styled-components'
 import App from './components/App';
 import * as serviceWorker from './util/serviceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+const GlobalStyle = createGlobalStyle`
+  html, body {
+    margin: 0;
+    padding: 0;
+    height: 100%;
+    font-family: 'Varela Round' !important;
+    box-sizing: border-box;
+  }
+`
 
+ReactDOM.render(
+  <React.Fragment>
+    <GlobalStyle />
+    <App />
+  </React.Fragment>,
+  document.getElementById('root')
+);
 serviceWorker.unregister();
